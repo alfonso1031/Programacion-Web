29d4c9b917a6c668e913378e67a25887838d6fcd
Alfonso Arroyo <arroyoalfonso1031@gmail.com>
2025-05-11 19:55:02 -0500

    portafolio update

---
+++ Primer Parcial/PRY_TEMPLADES/public/js/load.js
@@ -0,0 +1,17 @@
+//cargar el archivo header.html
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header').innerHTML = data)
+
+//cargar el archivo footer.html
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer').innerHTML = data)
+
+function cargarPaginas(url){
+    fetch(`paginas/${url}.html`)
+        .then(response => response.text())
+        .then(data => document.getElementById('main').innerHTML = data)
+}
+
+window.onload = () => cargarPaginas('index')
