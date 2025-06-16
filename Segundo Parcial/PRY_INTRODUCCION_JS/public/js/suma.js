b82bd1a7c535347f8d32d0b65deb9010f301d9bd
Alfonso Arroyo <arroyoalfonso1031@gmail.com>
2025-06-16 10:40:10 -0500

    sumar

---
+++ Segundo Parcial/PRY_INTRODUCCION_JS/public/js/suma.js
@@ -0,0 +1,6 @@
+function sumar(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let resultado = num1 + num2;
+    console.log("El resultado de la suma es: " + resultado);
+}
